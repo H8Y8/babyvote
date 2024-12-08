0347c5f4a7f3d5c7441f7eedd315fefa9409f436
H8Y8 <jayv63@hotmail.com>
2024-12-09 00:25:48 +0800

    更正監聽所有網路接口與修改映射端口

--- app.py
+++ app.py
@@ -336,4 +336,4 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
